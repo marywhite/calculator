31738db42a8935f97e2bf88aaa3e888a9ede36b5
Mary White <maryk.december@gmail.com>
2015-05-13 17:59:03 -0500

    display total

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,12 +1,37 @@
 var total
     , current = ''
-    , operator
-    , lastType;
+    , operator;
 
 $(document).ready(function(){
+
+    $('.equals').click(function(){
+        current = parseInt(current);
+        switch (operator) {
+            case "divide":
+                total /= current;
+                break;
+            case "times":
+                total *= current;
+                break;
+            case "minus":
+                total -= current;
+                break;
+            case "add":
+                total += current;
+        }
+        $('.numDisplay').text(total);
+    });
+
+    $('.operator').click(function(){
+        total = parseInt(current);
+        current = '';
+        operator = $(this).attr('id');
+        console.log(total, current);
+    });
+
     $('.num').click(function(){
         current += $(this).attr('id');
-        console.log(current);
+        $('.numDisplay').text(current);
     })
 
 });
